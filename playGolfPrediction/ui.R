8fef438e7a2592cac6457e62e5527f999b12390a
Willianto Asalim <wasalim@hotmail.com>
2020-07-30 01:51:52 +1000

    adding panel

--- playGolfPrediction/ui.R
+++ playGolfPrediction/ui.R
@@ -13,34 +13,47 @@ library(shinythemes)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(theme = shinytheme("united"),
 
+                  navbarPage(title = "Play Golf?",
+                      tabPanel("Main", 
+                               HTML("<h3>Input parameters</h3>"),
+                               
+                               selectInput("outlook", label = "Outlook:", 
+                                           choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
+                                           selected = "Rainy"),
+                               sliderInput("temperature", "Temperature:",
+                                           min = 64, max = 86,
+                                           value = 70),
+                               sliderInput("humidity", "Humidity:",
+                                           min = 65, max = 96,
+                                           value = 90),
+                               selectInput("windy", label = "Windy:", 
+                                           choices = list("Yes" = "TRUE", "No" = "FALSE"), 
+                                           selected = "TRUE"),
+                               
+                               actionButton("submitbutton", "Submit", class = "btn btn-primary"),
+                               
+                               tags$br(),
+                               
+                               tags$label(h3('Status/Output')), # Status/Output Text Box
+                               verbatimTextOutput('contents'),
+                               tableOutput('tabledata') # Prediction results table
+                               ),
+                      
+                      tabPanel("How to use"),
+                      tabPanel("How it works")
+                      
+                  )    
                   # Page header
-                  headerPanel('Play Golf?'),
+##                  headerPanel('Play Golf?'),
                   
                   # Input values
-                  sidebarPanel(
-                      HTML("<h3>Input parameters</h3>"),
+##                  sidebarPanel(
                       
-                      selectInput("outlook", label = "Outlook:", 
-                                  choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
-                                  selected = "Rainy"),
-                      sliderInput("temperature", "Temperature:",
-                                  min = 64, max = 86,
-                                  value = 70),
-                      sliderInput("humidity", "Humidity:",
-                                  min = 65, max = 96,
-                                  value = 90),
-                      selectInput("windy", label = "Windy:", 
-                                  choices = list("Yes" = "TRUE", "No" = "FALSE"), 
-                                  selected = "TRUE"),
-                      
-                      actionButton("submitbutton", "Submit", class = "btn btn-primary")
-                  ),
+##                  ),
                   
-                  mainPanel(
-                      tags$label(h3('Status/Output')), # Status/Output Text Box
-                      verbatimTextOutput('contents'),
-                      tableOutput('tabledata') # Prediction results table
+##                  mainPanel(
                       
-                  )
+                      
+##                  )
     )
 )
