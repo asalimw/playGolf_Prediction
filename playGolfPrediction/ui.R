3c6f15cf13a53ad3000e3d0d2d3a6108fb8d65ec
Willianto Asalim <wasalim@hotmail.com>
2020-07-30 03:01:04 +1000

    adding info

--- playGolfPrediction/ui.R
+++ playGolfPrediction/ui.R
@@ -20,10 +20,10 @@ shinyUI(fluidPage(theme = shinytheme("united"),
                                selectInput("outlook", label = "Outlook:", 
                                            choices = list("Sunny" = "sunny", "Overcast" = "overcast", "Rainy" = "rainy"), 
                                            selected = "Rainy"),
-                               sliderInput("temperature", "Temperature:",
+                               sliderInput("temperature", "Temperature (F):",
                                            min = 64, max = 86,
                                            value = 70),
-                               sliderInput("humidity", "Humidity:",
+                               sliderInput("humidity", "Humidity (%):",
                                            min = 65, max = 96,
                                            value = 90),
                                selectInput("windy", label = "Windy:", 
@@ -33,27 +33,50 @@ shinyUI(fluidPage(theme = shinytheme("united"),
                                actionButton("submitbutton", "Submit", class = "btn btn-primary"),
                                
                                tags$br(),
+                               tags$hr(),
                                
-                               tags$label(h3('Status/Output')), # Status/Output Text Box
+                               tags$label(h3('Should I play?')), # Status/Output Text Box
                                verbatimTextOutput('contents'),
                                tableOutput('tabledata') # Prediction results table
                                ),
                       
-                      tabPanel("How to use"),
-                      tabPanel("How it works")
+                      tabPanel("How to use",
+                               tags$label(h4("Unfavourable weather will affect your golf day!")),
+                               tags$br(),
+                               tags$body(h5("To determine whether it is a good day for you to play golf, you will need to select \
+                                            the following weather condition in the main page")),
+                               tags$br(),
+                               tags$ol(
+                                   tags$li("Outlook - Sunny, Overcast or Rainy"), 
+                                   tags$li("Temperature in Fahrenheit"), 
+                                   tags$li("Humidity"),
+                                   tags$li("Windy - Yes or No"),
+                                      )
+                            ),
+                      tabPanel("How it works",
+                               tags$label(h3("Prediction Methodology")),
+                               tags$br(),
+                               tags$div(
+                                   tags$p("There absolutely is such a thing as golf weather, 
+                                          although that specific spectrum of perfectly playable weather conditions depends on where you live, 
+                                          how creaky your body gets, what time of the year it is, 
+                                          and how dedicated you are to the sport. 
+                                          A vast majority of dedicated golfers love cooler weather with low humidity and no breeze 
+                                          (or just a little bit of a breeze)."), 
+                                   
+                                   tags$p("The method used to predict the ideal golf day is to use Random Forest. 
+                                          Random forests or random decision forests are an ensemble learning method for classification, 
+                                          regression and other tasks that operate by constructing a multitude of decision trees at training time 
+                                          and outputting the class that is the mode of the classes (classification) or mean prediction (regression) 
+                                          of the individual trees."), 
+                                   tags$p("The weather dataset used contains 14 examples of weather condition
+                                          suitable for playing a game of golf. The info on this data set can be found here:"),
+                                   tags$a(href="https://datacadamia.com/data_mining/weather", "Click here!")
+                               )
+                        )
+                               
                       
-                  )    
-                  # Page header
-##                  headerPanel('Play Golf?'),
-                  
-                  # Input values
-##                  sidebarPanel(
-                      
-##                  ),
-                  
-##                  mainPanel(
-                      
-                      
-##                  )
+                                
+                  )
     )
 )
